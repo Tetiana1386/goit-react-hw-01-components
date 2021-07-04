84299f779c3fbd984603ccf6917858dd6a5a573c
Tetiana1386 <tanyashostak13@gmail.com>
2021-07-05 00:59:11 +0300

    Додає профіль без стилізації

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import user from './user.json';
+
+const App = () => {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+};
+
+export default App;
