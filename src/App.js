ad990bdd528ac9e2969f2576bf84f88fb904b353
Tetiana1386 <tanyashostak13@gmail.com>
2021-07-06 01:29:19 +0300

    Додає інші компоненти без стилізації

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+
 import Profile from './components/Profile/Profile';
-import user from './user.json';
+import user from './components/Profile/user.json';
+
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendListItem/friends.json';
+
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 const App = () => {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
 
